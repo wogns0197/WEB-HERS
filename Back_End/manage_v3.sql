44d0e8db08b0722a26972bff44c85af4ddec03db
MilyangParkJaeHoon <jh9277@naver.com>
2017-11-20 23:29:34 +0900

    insert_schema

--- Back_End/manage_v3.sql
+++ Back_End/manage_v3.sql
@@ -10,15 +10,16 @@ create table user
 
     primary key(user_id)
 );
-create table footsal_manage
+create table futsal_manage
 (
   manage_ID     integer(10) not null default 1,
   user_id       varchar(10),
   borrowdate    date,
-  start_time    date,
-  end_time      date,
+  start_time    time,
+  end_time      time,
   place         varchar(10),
   purpose       varchar(30),
+  notice        boolean,
   people        integer(5),
   groupname     varchar(10),
   primary key(manage_ID,borrowdate),
@@ -30,8 +31,8 @@ create table purpose_view
     manage_ID   integer(10),
     purpose     varchar(30),
     borrowdate  date,
-    start_time  date,
-    end_time    date
+    start_time  time,
+    end_time    time,
     primary key(manage_ID,borrowdate)
 );
 create table lec_room_manage
@@ -39,8 +40,8 @@ create table lec_room_manage
   manage_ID     integer(10),
   user_id       varchar(10),
   borrowdate    date,
-  start_time    date,
-  end_time      date,
+  start_time    time,
+  end_time      time,
   place         varchar(20),
   purpose       varchar(30),
   people        integer(5),
@@ -49,4 +50,23 @@ create table lec_room_manage
   foreign key(user_id) references user(user_id)
     on delete set null
 );
+insert into user (name,student_ID, phone_num, e_mail
+,user_id, user_pw, dept_name)
+values ("parkjaehoon",2016003509, 010,"djdj","jaehoon","2345","sw");
+insert into user (name,student_ID, phone_num, e_mail
+,user_id, user_pw, dept_name)
+values ("hoonoo",2016003508, 010,"djdj","hahah","2345","sw");
 
+insert into futsal_manage (user_id, borrowdate, start_time, end_time, place,
+purpose, notice, people, groupname)
+values ("jaehoon","2017-02-03","12:00:00","14:00:00","대운동장",
+"소프트 ict", true,20, "park");
+insert into futsal_manage (user_id, borrowdate, start_time, end_time, place,
+purpose, notice, people, groupname)
+values ("hahah","2017-06-03","14:00:00","15:00:00","대운동장",
+"전자 ict", true,20, "hoon");
+
+insert into purpose_view
+values (1, "소프트 ict", "2017-02-03","12:00:00","14:00:00");
+insert into purpose_view
+values (2, "전자 ict", "2017-02-03","12:00:00","14:00:00");
