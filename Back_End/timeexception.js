c6aa9cabff88d9501aad75c5dd7773a66f5dcfef
chlwodud77 <uniform3000@naver.com>
2017-11-30 10:41:51 +0900

    fix ontime check js

--- Back_End/timeexception.js
+++ Back_End/timeexception.js
@@ -1,10 +1,12 @@
 window.onload = check;
 function check(){
-    var value = $("time").value;
+    var value = document.getElementById("time").value;
     var time = value.split(" ");
     if(time[0] == "" || time[0] == null){
         alert("예약 시간을 입력해주세요");
+    	
         window.history.goback();
+
     }
     alert("ss");
 }
