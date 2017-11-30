bdb3a7c6804cd2edcd2268bdef8acb742ac96d0b
chlwodud77 <uniform3000@naver.com>
2017-11-30 22:26:18 +0900

    fix timeexception2 error.

--- Back_End/timeexception.js
+++ Back_End/timeexception.js
@@ -1,13 +1,16 @@
 var flag=false;
 window.onload = function(){
     document.getElementById("button").onclick = check;
-    document.getElementsByClassName("time").onclick = check2;
+    var times = document.getElementsByClassName("time");
+    for(var i = 0; i< times.length; i++){
+    	times[i].onclick = check2;
+    }
 };
 
 function check2(){
-    alert("aa");
     flag=true;
 }
+
 function check(event){
     if(!flag){
         alert("시간을 입력해주세요");
