d7f00c15827abd050b3975e9d8a8d363dfbc2ff4
chlwodud77 <uniform3000@naver.com>
2017-12-12 15:43:10 +0900

    other fix success_send_message

--- Back_End/success_send_message.js
+++ Back_End/success_send_message.js
@@ -1,9 +1,4 @@
 window.onload = function(){
     alert("메세지가 성공적으로 보내졌습니다!");
-    goTo();
-}
-
-
-function goTo(){
-  location.href="http://www.naver.com";
+    location.href="../Front_End/futsal/futmain2.php";    
 }
