93299cc7ff7efec83a2519792cadae1d5bc39c1c
chlwodud77 <uniform3000@naver.com>
2017-12-17 00:42:17 +0900

    add find id by using ajax

--- Back_End/login_function/find_id.php
+++ Back_End/login_function/find_id.php
@@ -1,76 +1,46 @@
-    
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Find Id</title>
-	<meta charset="utf-8"/>
-</head>
 
-
-
-<body>
 <?php
-	$name = $_POST['name'];
-	$s_id = $_POST['sid'];
+	$name = $_GET['name'];
+	$s_id = $_GET['sid'];
 	$db_name = 'web_project';
 	$find = true;
 
 	//이름과 학번으로 아이디를 찾는 쿼리
 	$query = "select user_id from user where name = '$name' and student_ID = '$s_id'";
-	
-	//이름과 학번으로 아이디 개수를 찾는 쿼리
-	$query2 = "select count(user_id) from user where name = '$name' and student_ID = '$s_id'";
 
     $db = new PDO("mysql:dbname=$db_name", "root","root");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   	try{
-	    $id_counts = $db->query($query2); //이름과 학번으로 아이디 개수를 찾는 쿼리
+
+		    $rows = $db->query($query);
+
+				$idlists = [];
 
 
-	    foreach($id_counts as $counts){ //아이디 개수가 0인지 아닌지 확인
-	    	if($counts['count(user_id)'] == 0){
-	    		$find = false;
-	    	}
+			  foreach($rows as $row){
+					$idlists[] = $row['user_id'];
+			  }
 
-	    }
 
-	    if($find === false){ //아이디 개수가 0이면, 즉 아이디가 없으면
-	    ?>
-	    	<p>회원님의 아이디를 찾을 수 없습니다. 이름과 학번을 확인해주세요.</p>
-	    <?php
-	    }
-	    else{ //아이디가 있으면
-	    $rows = $db->query($query);
+				$data = array();
+				$lists = array();
+				foreach($idlists as $row){
+					$id = array('id' => "$row");
+					array_push($lists,$id);
+				}
 
-		?>
-		    <p>회원님의 아이디 목록입니다.</p>
-		    <?php
+				$data["idlists"] = $lists;
 
-		    foreach($rows as $row){
-		    ?>
-		    	<p><span><?= $row['user_id'] ?></span></p>
+				header("Content-type: application/json");
+				print json_encode($data);
 
-		    <?php
-		    }
-	    }
 
-	    
-	}
-	catch(PDOException $ex){
 
-		echo "detail: ".$ex->getMessage();
-	} 
-    
+		}
+		catch(PDOException $ex){
+
+			echo "detail: ".$ex->getMessage();
+		}
 
 ?>
-
-<a href = '../../Front_End/futsal/futmain2.php'><button>메인으로</button></a>
-
-
-
-</body>
-
-</html>
-
-
