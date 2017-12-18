de7fcfecf2a438b6d2ad0b0b4da4d8088f0df895
chlwodud77 <uniform3000@naver.com>
2017-12-18 20:55:02 +0900

    fix about page menu link

--- apps/about/about.php
+++ apps/about/about.php
@@ -64,9 +64,9 @@ session_start();
         <dt>RENTAL</dt>
         <dd>
           <ul class="menus">
-            <li><a href="../futsal_confirmation/reservation_lists.php">Futsal Confirmation</a></li>
-            <li><a href="../futsal/futmain2.php">Futsal Field Rental</a></li>
-            <li><a href="../mypage/match.php">Matching Request</a></li>
+            <li><a href="../list/reservation_lists.php">Futsal Confirmation</a></li>
+            <li><a href="../futsal_main/futmain.php">Futsal Field Rental</a></li>
+            <li><a href="../matching/match.php">Matching Request</a></li>
           </ul>
         </dd>
       </dl>
